3ea8be970be6706c9ed14ba726a747d03021af25
catvlad <catvlad@hotmail.com>
2021-12-20 12:53:48 +0000

    start 2

--- internal/server.go
+++ internal/server.go
@@ -1,7 +1,25 @@
+package main
+
 import (
-	"http"
+	// "fmt"
+	// "io"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
 )
 
-func main() {
+func HelloServer(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte("This is an example server.\n"))
+	fmt.Fprintf(w, "This is an example server.\n")
+	io.WriteString(w, "This is an example server.\n")
+}
 
+func main() {
+	http.HandleFunc("/", HelloServer)
+	err := http.ListenAndServeTLS(":443", "server.crt", "server.key", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
